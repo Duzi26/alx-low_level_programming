2b1a2a436aed225a0eabf7588bf79dea9c849ad8
Duzi26 <duzidaniel@yahoo.com>
2022-09-08 23:48:52 -0400

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - printing.z
+ *
+ * Return: Always 0 (is scuccess)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return(0)
+}
