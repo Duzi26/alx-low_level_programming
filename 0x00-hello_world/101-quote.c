40b57d4a2ed59d3d793e22d540df4b8137001f2f
Duzi26 <duzidaniel@yahoo.com>
2022-09-09 01:29:38 -0400

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+/**
+ *
+ *prints messages[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D main - [C[C[Cmain - prints messages
+ *
+ *Return: Always 1 (success)
+ */
+int main(void)
+{                                                                                    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+     return (1);
