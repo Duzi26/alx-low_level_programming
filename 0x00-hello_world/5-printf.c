5fe44b951bf87b80923308d4413785a2e99db51e
Duzi26 <duzidaniel@yahoo.com>
2022-09-09 01:18:03 -0400

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - Starting point
+ * main - prints messages
  *
- * Return: Always 0 ( if successful)
+ * Return: Always 0 (Success)
  */
 int main(void)
-{                                                                                            [D[D[D[D[D[D[D[Cprintf("with proper grammar, but the outcome is a piece of art,\n");
-       return (0);
+{                                                                                    printf("with proper grammar, but the outcome is a piece of art,\n");
+     return (0);
 }
